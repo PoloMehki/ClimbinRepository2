7ad29e677709591f81db4b07fc3d61441223dbe1
Ruthie L <ruthielar14@gmail.com>
2023-04-26 12:37:34 -0700

    Created serialized fields to input duration of levels
    Level 1 only works. Level 2 & 3 in progress ~ working on how to make them consecutive.

--- Assets/Scripts/TimerDemo.cs
+++ Assets/Scripts/TimerDemo.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class TimerDemo : MonoBehaviour
 {
-    [SerializeField] Timer timer1;
+    [SerializeField] Timer timer1; //create timer
+    [SerializeField] int l1Duration; //duration of Level 1
+    [SerializeField] int l2Duration; //duration of Level 2
+    [SerializeField] int l3Duration; //duration of Level 3
     // Start is called before the first frame update
     private void Start()
     {
-        timer1.SetDuration (7).Begin ();
+        timer1.SetDuration (l1Duration).Begin ();
+        
     }
 
     // Update is called once per frame
