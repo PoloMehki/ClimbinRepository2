7220b654d502ef071792465a1fb14cf242e1c8d3
Ruthie L <ruthielar14@gmail.com>
2023-04-26 11:36:30 -0700

    Added Keyboard Functionality - Mehki

--- Assets/Scripts/script.cs
+++ Assets/Scripts/script.cs
@@ -15,6 +15,41 @@ public class script : MonoBehaviour
     [SerializeField] GameObject letterButton; //The button pressed to select a letter
     [SerializeField] TextAsset possibleWord; //the word the player is attempting to guess
 
+    //LETTER VARS
+    bool ban, ana, can, dan, ear, fat, gat, hin, ing, jin, kin, lin, mde, nop, oer, por, qur, ret, ste, tub, unb, ver, wea, xas, yus, zus;
+
+
+    private void ResetKeyboard(){
+        //ban, ana, can, dan, ear, fat, gat, hin, ing, jin, kin, lin, mde, nop, oer, por, qur, ret, ste, tub, unb, ver, wea, xas, yus, zus
+        ban = false;
+        ana = false;
+        can = false;
+        dan = false;
+        ear = false;
+        fat = false;
+        gat = false;
+        hin = false;
+        ing = false;
+        jin = false;
+        kin = false;
+        lin = false;
+        mde = false;
+        nop = false;
+        oer = false;
+        por = false;
+        qur = false;
+        ret = false;
+        ste = false;
+        tub = false;
+        unb = false;
+        ver = false;
+        wea = false;
+        xas = false;
+        yus = false;
+        zus = false;
+
+    }
+
     private string word; //variable to represent the word
 
     private int incorrect; //Counts the incorrect guesses
@@ -54,6 +89,8 @@ public class script : MonoBehaviour
         foreach(char letter in word){
             var temp = Instantiate(letterContainer, wordbox.transform);
         }
+        ResetKeyboard();
+        
     }
 
     //Makes a button with the input i reperesnting an ascii character
@@ -68,7 +105,7 @@ public class script : MonoBehaviour
     private string generateWord(){
         string[] wordList = possibleWord.text.Split('\n');
         string line = wordList[Random.Range(0, wordList.Length -1)];
-        return line.Substring(0, line.Length -1);
+        return line.Substring(0, line.Length-1);
     }
     
     //When a letter is pressed, this will check whether that letter is found in the word the player is attempting to guess
@@ -112,6 +149,212 @@ public class script : MonoBehaviour
             }
             //restarts the game
             Invoke("InitializeGame", 3f);
+        }
+    }
+    void Update(){
+        //List: ban, ana, can, dan, ear, fat, gat, hin, ing, jin, kin, lin, mde, nop, oer, por, qur, ret, ste, tub, unb, ver, wea, xas, yus, zus
+        if(Input.GetKeyDown(KeyCode.A)){
+            Debug.Log("A was pressed");
+            if(ana == false){
+            CheckLetter("A");
+            Debug.Log("BANANALOCK");
+            ana = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.B)){
+            Debug.Log("B was pressed");
+            if(ban == false){
+            CheckLetter("B");
+            Debug.Log("BANANALOCK");
+            ban = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.C)){
+            Debug.Log("C was pressed");
+            if(can == false){
+            CheckLetter("C");
+            Debug.Log("BANANALOCK");
+            can = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.D)){
+            Debug.Log("D was pressed");
+            if(dan == false){
+            CheckLetter("D");
+            Debug.Log("BANANALOCK");
+            dan = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.E)){
+            Debug.Log("E was pressed");
+            if(ear == false){
+            CheckLetter("E");
+            Debug.Log("BANANALOCK");
+            ear = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.F)){
+            Debug.Log("F was pressed");
+            if(fat == false){
+            CheckLetter("F");
+            Debug.Log("BANANALOCK");
+            fat = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.G)){
+            Debug.Log("G was pressed");
+            if(gat == false){
+            CheckLetter("G");
+            Debug.Log("BANANALOCK");
+            gat = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.H)){
+            Debug.Log("H was pressed");
+            if(hin == false){
+            CheckLetter("H");
+            Debug.Log("BANANALOCK");
+            hin = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.I)){
+            Debug.Log("I was pressed");
+            if(ing == false){
+            CheckLetter("I");
+            Debug.Log("BANANALOCK");
+            ing = true;}
+        }
+        if(Input.GetKeyDown(KeyCode.J)){
+            Debug.Log("J was pressed");
+            if(jin == false){
+            CheckLetter("J");
+            Debug.Log("BANANALOCK");
+            jin = true;}
+        }
+        if(Input.GetKeyDown(KeyCode.K)){
+            Debug.Log("K was pressed");
+            if(kin == false){
+            CheckLetter("K");
+            Debug.Log("BANANALOCK");
+            kin = true;}
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            Debug.Log("L was pressed");
+            if(lin == false){
+            CheckLetter("L");
+            Debug.Log("BANANALOCK");
+            lin = true;}
+        }
+        if(Input.GetKeyDown(KeyCode.M)){
+            Debug.Log("M was pressed");
+            if(mde == false){
+            CheckLetter("M");
+            Debug.Log("BANANALOCK");
+            mde = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.N)){
+            Debug.Log("N was pressed");
+            if(nop == false){
+            CheckLetter("N");
+            Debug.Log("BANANALOCK");
+            nop = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.O)){
+            Debug.Log("O was pressed");
+            if(oer == false){
+            CheckLetter("O");
+            Debug.Log("BANANALOCK");
+            oer = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.P)){
+            Debug.Log("P was pressed");
+            if(por == false){
+            CheckLetter("P");
+            Debug.Log("BANANALOCK");
+            por = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q)){
+            Debug.Log("Q was pressed");
+            if(qur == false){
+            CheckLetter("Q");
+            Debug.Log("BANANALOCK");
+            qur = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            Debug.Log("R was pressed");
+            if(ret == false){
+            CheckLetter("R");
+            Debug.Log("BANANALOCK");
+            ret = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.S)){
+            Debug.Log("S was pressed");
+            if(ste == false){
+            CheckLetter("S");
+            Debug.Log("BANANALOCK");
+            ste = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.T)){
+            Debug.Log("T was pressed");
+            if(tub == false){
+            CheckLetter("T");
+            Debug.Log("BANANALOCK");
+            tub = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.U)){
+            Debug.Log("U was pressed");
+            if(unb == false){
+            CheckLetter("U");
+            Debug.Log("BANANALOCK");
+            unb = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.V)){
+            Debug.Log("V was pressed");
+            if(ver == false){
+            CheckLetter("V");
+            Debug.Log("BANANALOCK");
+            ver = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("W was pressed");
+            if(wea == false){
+            CheckLetter("W");
+            Debug.Log("BANANALOCK");
+            wea = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.X)){
+            Debug.Log("X was pressed");
+            if(xas == false){
+            CheckLetter("X");
+            Debug.Log("BANANALOCK");
+            xas = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.Y)){
+            Debug.Log("Y was pressed");
+            if(yus == false){
+            CheckLetter("Y");
+            Debug.Log("BANANALOCK");
+            yus = true;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z)){
+            Debug.Log("Z was pressed");
+            if(zus == false){
+            CheckLetter("Z");
+            Debug.Log("BANANALOCK");
+            zus = true;}
         }
     }
 }
