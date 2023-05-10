6650422ffbd8a18cc281eb8fa08fbe7e504c0189
Abby <abbyapple23@gmail.com>
2023-05-10 11:45:49 -0700

    more Score Manager
    I tried to connect the scoreText object to the text on the screen. I do not have the updated screens on my unity, don't know why, the next step will be to fix that. Some bugs I have are about objects not being found and a reference being required for a non-static field.

--- Assets/Abby/ScoreManager.cs
+++ Assets/Abby/ScoreManager.cs
@@ -7,40 +7,40 @@ using System.Text;
 public class ScoreManager : MonoBehaviour
 {
 
-    private string scoreText;
+    public GameObject scoreText;
 
     public int score = 0;
 
     // Start is called before the first frame updates
     void Start()
     {
-        scoreText = score.ToString(); 
+        scoreText.GetComponent<scoreText>().text = score.ToString(); 
     }
 
     // adds points for each letter guessed correctly 
-    public static string addPoints()
+    public void addPoints()
     {
 	    score += 1000;
-        return "SCORE: " + score.ToString();
+        scoreText.text = "SCORE: " + score.ToString();
     }
 
     // subtracts points for each letter guessed incorrectly
-    public static string subtractPoints()
+    public void subtractPoints()
     {
         score -= 250;
-        return "SCORE: " + score.ToString();
+        scoreText.text = "SCORE: " + score.ToString();
     }
 
     // adds points for each word guessed correctly 
-    public static string correctWord()
+    public void correctWord()
     {
         score += 10000;
-        return "SCORE: " + score.ToString();
+        scoreText.text = "SCORE: " + score.ToString();
     }
-
+    /* 
     public override string ToString()
     {
         return score;
     }
-
+    */
 }
