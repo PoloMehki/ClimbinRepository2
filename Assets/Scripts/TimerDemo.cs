70ff446910651f1d7ad8dbe3d53cdfd957c79359
Ruthie L <ruthielar14@gmail.com>
2023-05-19 13:56:05 -0700

    timer function
    scrapped the idea for the consecutive timers.

--- Assets/Scripts/TimerDemo.cs
+++ Assets/Scripts/TimerDemo.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class TimerDemo : MonoBehaviour
 {
-    [SerializeField] Timer timer1; //create timer
-    [SerializeField] int l1Duration; //duration of Level 1
-    [SerializeField] int l2Duration; //duration of Level 2
-    [SerializeField] int l3Duration; //duration of Level 3
-    // Start is called before the first frame update
-    private void Start()
-    {
-        timer1
-        .SetDuration (l1Duration)
-        .OnBegin(() => Debug.Log ("Timer started")) 
+    [SerializeField] Timer timer; //create timer
+    public int timerDuration; //duration of Level 3
+    
+
+    public void Start() {
+        startTimer(timerDuration);
+    }
+
+    public void startTimer(int duration){
+        timer
+        .SetDuration (duration)
+        .OnBegin(() => Debug.Log ("Timer started")) //when timer starts then output debug message
         .OnChange((remainingSeconds) => Debug.Log ("Timer changed : " + remainingSeconds)) //takes in an int
-        .OnEnd(() => Debug.Log ("Timer ended")) 
+        .OnEnd(() => Debug.Log ("Timer ended"))
+        //.OnPause(paused => Debug.Log("-------Paused : " + isPaused))
         .Begin ();
-        
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
         
     }
+
 }
