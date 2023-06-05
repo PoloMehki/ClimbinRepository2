9d4f8ece7de434e224efc617b5840b7d961a9a19
Ruthie L <ruthielar14@gmail.com>
2023-06-05 11:14:29 -0700

    Scene switches to EndScene when timer ends
    - Deleted the Host & Client Scenes
    - created EndScene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -11,16 +11,20 @@ public class Timer : MonoBehaviour
     [SerializeField] private Image uiFillImage; //the image filling/decreasing
     [SerializeField] private TMP_Text uiText; //timer text (minutes & seconds)
 
+
     public int Duration {get; private set;}
     //public bool IsPaused {get; private set;} COMMENTED BECAUSE PAUSE FUNCTION IS IN OWN SCRIPT
 
-    private int remainingDuration; //the current time
+    public int remainingDuration; //the current time
 
     //EVENTS
     private UnityAction onTimerBeginAction ;
     private UnityAction <int> onTimerChangeAction;
-    private UnityAction onTimerEndAction ;
+    public UnityAction onTimerEndAction ;
     //private UnityAction <bool> onTimerPausedAction ;
+
+    //Scene instance
+    //ChangeScene endScene = AddComponent<ChangeScene>(gameObject);
 
     
     private void Awake () {
@@ -28,7 +32,7 @@ public class Timer : MonoBehaviour
     }
 
     //Restarting Timer at 00:00
-    private void ResetTimer (){
+    public void ResetTimer (){
         uiText.text = "00:00";
         
         uiFillImage.fillAmount = 0f;
@@ -121,7 +125,7 @@ public class Timer : MonoBehaviour
     }*/
 
     //Changes the text and fill image as timer moves doown
-    private void UpdateUI(int seconds) {
+    public void UpdateUI(int seconds) {
         uiText.text = string.Format("{0:D2}:{1:D2}", seconds / 60, seconds % 60); //d2 Means print 2 digits even if the seconds are more than 10
         uiFillImage.fillAmount = Mathf.InverseLerp (0, Duration, seconds);
             //inverselerp returns a value between 0 & 1 depending on the value e.g. btw 0 & 10 value is 5
@@ -131,8 +135,10 @@ public class Timer : MonoBehaviour
         if (onTimerEndAction != null){
             onTimerEndAction.Invoke();
         }
+        
         StopAllCoroutines ();
         ResetTimer ();
+
     }
 
     private void OnDestroy() {
@@ -141,72 +147,3 @@ public class Timer : MonoBehaviour
 
 }
 
-//-----
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.Events;
-// using UnityEngine.UI;
-// using TMPro;
-
-// public class Timer : MonoBehaviour
-// {
-//     [Header("Timer UI Refrences :")]
-//     [SerializeField] private Image uiFillImage; //the image filling/decreasing
-//     [SerializeField] private TMP_Text uiText;
-
-//     public int Duration {get; private set;} //read only 
-
-//     private int remainingDuration; //the current time
-
-//     private void Awake () {
-//         ResetTimer ();
-//     }
-
-//     //Restarting Timer at 00:00
-//     private void ResetTimer (){
-//         uiText.text = "00:00";
-        
-//         uiFillImage.fillAmount = 0f;
-
-//         Duration = remainingDuration = 0;
-//     }
-
-//     //Sets the timer to a specific time
-//     public Timer SetDuration(int seconds) {
-//         Duration = remainingDuration = seconds;
-//         return this;
-//     }
-
-//     //Starts the timer
-//     public void Begin () {
-//         StopAllCoroutines();
-//         StartCoroutine(UpdateTimer ());
-//     }
-
-//     //Updates the Timer as it decreases
-//     private IEnumerator UpdateTimer(){
-//         while(remainingDuration > 0){
-//             UpdateUI (remainingDuration);
-//             remainingDuration--;
-//             yield return new WaitForSeconds (1f);
-//         }
-//         End();
-//     }
-
-//     //Changes the text and fill image as timer moves doown
-//     private void UpdateUI(int seconds) {
-//         uiText.text = string.Format("{0:D2}:{1:D2}", seconds / 60, seconds % 60); //d2 Means print 2 digits even if the seconds are more than 10
-//         uiFillImage.fillAmount = Mathf.InverseLerp (0, Duration, seconds);
-//             //inverselerp returns a value between 0 & 1 depending on the value e.g. btw 0 & 10 value is 5
-//     }
-
-//     public void End () {
-//         ResetTimer ();
-//     }
-
-//     private void OnDestroy() {
-//         StopAllCoroutines ();
-//     }
-
-// }
--- Assets/Scripts/TimerDemo.cs
+++ Assets/Scripts/TimerDemo.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimerDemo : MonoBehaviour
 {
     [SerializeField] Timer timer; //create timer
     public int timerDuration; //duration of Level 3
-    
+
+
 
     public void Start() {
         startTimer(timerDuration);
+        
+        //toEndScene.MoveToScene(2);
+        //endScene.MoveToScene(2);
+
     }
 
     public void startTimer(int duration){
@@ -18,13 +24,16 @@ public class TimerDemo : MonoBehaviour
         .OnBegin(() => Debug.Log ("Timer started")) //when timer starts then output debug message
         .OnChange((remainingSeconds) => Debug.Log ("Timer changed : " + remainingSeconds)) //takes in an int
         .OnEnd(() => Debug.Log ("Timer ended"))
-        //.OnPause(paused => Debug.Log("-------Paused : " + isPaused))
         .Begin ();
     }
 
     // Update is called once per frame
     private void Update() {
-        
+        if(timer.remainingDuration == 0){
+            SceneManager.LoadScene(2);
+        }
+
+        //uiText.text
     }
 
 }
